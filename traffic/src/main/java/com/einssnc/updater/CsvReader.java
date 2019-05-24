1707083d5a91cbf0bcb8d15714d1bd986bb5a7aa
woojh3690 <woojh3690@gmail.com>
2019-05-24 13:59:08 +0900

    멀티쓰레드 insert

--- traffic/src/main/java/com/einssnc/updater/CsvReader.java
+++ traffic/src/main/java/com/einssnc/updater/CsvReader.java
@@ -6,51 +6,112 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import com.opencsv.CSVReader;
 
-public class CsvReader {
+public class CsvReader extends Thread {
 	
-	private String[] row = null;
-	CSVReader reader = null;
+	private static LinkedBlockingQueue<String> q;
+	private static CSVReader reader = null;
+	private static int limit; 
+	private static int liveThread = 0;
 	
-	public CsvReader(String downloadPath) {	
+	private int name = 0;
+	
+	public CsvReader(int name) {
+		this.name = name;
+	}
+	
+	public static void init(String downloadPath, int alimit) {
+		q = new LinkedBlockingQueue<String>();
+		limit = alimit;
+		
 		FileInputStream steam;
 		try {
 			steam = new FileInputStream(downloadPath);
-			reader = new CSVReader(new InputStreamReader(
-					steam ,"EUC-KR"), ',', '"');
+			reader = new CSVReader(new InputStreamReader(steam ,"EUC-KR"), ',', '"');
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
-	
-	public List<String[]> readNext(int buffer) {
-		List<String[]> rows = new ArrayList<>(); 
-		
-		try {
-			row = reader.readNext();
-			if (row == null) {
-				return null;
-			}
-			for (int i = 0; (row = reader.readNext()) == null || i < buffer; i++) {
-				rows.add(row);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 
-		return rows;
+	
+	@Override
+	public void run() {
+		arrive();
+		String[] row = null;
+		
+		do {
+			try {
+				
+				String sql = "INSERT INTO `nation_wide_speed` VALUES ";
+				
+				for (int i = 0; i < limit; i++) {
+					synchronized (reader) {
+						row = reader.readNext();
+					}
+					if (row == null) {
+						break;
+					}
+					row[0] = "'" + row[0] + "'";
+					row[1] = "'" + row[1] + "'";
+					String query = Arrays.toString(row);
+					query = query.replace("[", "(").replace("]", "),\n");
+					sql += query;
+				}
+
+				sql = sql.substring(0, sql.length() - 2);
+				sql += ";";
+
+				q.offer(sql);
+				System.out.println(name + "-쓰레드 큐에 데이터 추가");
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} while (row != null);
+		
+//		if (row == null) {
+//			System.out.println("null 맞음");
+//		} else {
+//			System.out.println("null 아님");
+//		}
+		
+		dead();
 	}
 	
-	public void close() {
+	public static String qTake() throws InterruptedException {
+		return q.take();
+	}
+	
+	public static int qCheck() {
+		return q.size();
+	}
+	
+	public synchronized void arrive() {
+		liveThread += 1;
+		System.out.println(name + "-쓰레드 시작 현재 살아있는 쓰레드 수 : " + liveThread);
+	}
+	
+	public synchronized void dead() {
+		liveThread -= 1;
+		System.out.println(name + "-쓰레드 완료 현재 살아있는 쓰레드 수 : " + liveThread);
+	}
+	
+	public static int getLiveThread() {
+		return liveThread;
+	}
+	
+	public static void close() {
+		System.out.println("reader close");
 		try {
 			reader.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
--- traffic/src/main/java/com/einssnc/updater/MySqlUpdater.java
+++ traffic/src/main/java/com/einssnc/updater/MySqlUpdater.java
@@ -1,17 +1,14 @@
 package com.einssnc.updater;
 
-import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MySqlUpdater {
 
+	private int limit = 1000;
+	private int threadNum = 5;
+	
 	public MySqlUpdater() {
 		// TODO Auto-generated constructor stub
 	}
@@ -19,9 +16,8 @@ public class MySqlUpdater {
 	public void insert() {
 		Connection conn = null;
         Statement stmt = null;
-        CsvReader csvReader = null;
         
-        int rs;
+        Integer rs = null;
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -30,47 +26,36 @@ public class MySqlUpdater {
 					"woojh3690", 
 					"woojh1138!");
 			System.out.println("연결 완료");
-			
+
 			stmt = conn.createStatement();
+
+			CsvReader.init("C:/Temp/20190401_5Min.csv", limit);
 			
-			csvReader = new CsvReader("C:/Temp/20190401_5Min.csv");
-			List<String[]> rows = new ArrayList<String[]>();
-			
-			while ((rows = csvReader.readNext(100)) != null) {
-				String sql = "INSERT INTO `nation_wide_speed` VALUES ";
-				
-				for (String[] row : rows) {
-					row[0] = "'" + row[0] + "'";
-					row[1] = "'" + row[1] + "'";
-					String query = Arrays.toString(row);
-					query = query.replace("[", "(").replace("]", ")");
-					query += ",\n";
-					//System.out.println(query);
-					sql += query;
-				}
-				sql = sql.substring(0, sql.length() - 2);
-				sql += ";";
-				System.out.println(sql);
-				rs = stmt.executeUpdate(sql);
-				System.out.println(rs);
+			for (int i=0; i < threadNum; i++) {
+				new CsvReader(i+1).start();
 			}
+
+			do {
+				rs = stmt.executeUpdate(CsvReader.qTake());
+				System.out.println(rs + " : insert됨");
+			} while (CsvReader.qCheck() != 0 || CsvReader.getLiveThread() != 0);
+
 			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-            try{
-            	if( conn != null && !conn.isClosed()){
-                    conn.close();
-                }
-                if( stmt != null && !stmt.isClosed()){
-                    stmt.close();
-                }
-                csvReader.close();
-            }
-            catch( SQLException e){
-                e.printStackTrace();
-            }
-        }
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+				if (stmt != null && !stmt.isClosed()) {
+					stmt.close();
+				}
+				CsvReader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
